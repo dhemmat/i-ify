0fe3b97e9520baa3154d7bb813a6feabdc2a51e2
Geronimo Romney <g.romney@gmail.com>
2015-10-18 09:28:31 -0400

    use any vowel

--- lib/i_ify.rb
+++ lib/i_ify.rb
@@ -1,11 +1,10 @@
 require "i_ify/version"
 
 module I_ify
-  def self.i_ify text
+  def self.i_ify text, vowel = "i"
     puts "HERE!!!"
     puts text
-    text.gsub!(/[aeiou]/,"i")
-    text.gsub!(/[AEIOU]/,"I")
+    text.gsub!(/[aeiou]/,vowel.downcase)
+    text.gsub!(/[AEIOU]/,vowel.upcase)
   end
 end
-
--- spec/i_ify_spec.rb
+++ spec/i_ify_spec.rb
@@ -5,5 +5,13 @@ describe I_ify do
     it "substitutes every vowel with an 'i' or 'I'" do
       expect(I_ify::i_ify("I want to go watch Red October")).to eq "I wint ti gi witch Rid Ictibir"
     end
+
+    it "should use any vowel, 'i' by default" do
+      expect(I_ify::i_ify("Una Mosca Parada En La Pared", "a")).to eq "Ana Masca Parada An La Parad"
+      expect(I_ify::i_ify("Una Mosca Parada En La Pared", "e")).to eq "Ene Mesce Perede En Le Pered"
+      expect(I_ify::i_ify("Una Mosca Parada En La Pared")).to eq "Ini Misci Piridi In Li Pirid"
+      expect(I_ify::i_ify("Una Mosca Parada En La Pared", "o")).to eq "Ono Mosco Porodo On Lo Porod"
+      expect(I_ify::i_ify("Una Mosca Parada En La Pared", "u")).to eq "Unu Muscu Purudu Un Lu Purud"
+    end
   end
 end
